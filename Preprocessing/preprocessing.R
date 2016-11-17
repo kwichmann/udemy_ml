6aeec2ff64c5886842e11ef5443abb3abccac634
kwichmann <kristian.wichmann@gmail.com>
2016-11-18 00:09:11 +0100

    Imputation, training-test split, and feature scaling done

--- Preprocessing/preprocessing.R
+++ Preprocessing/preprocessing.R
@@ -3,5 +3,21 @@
 # Loading the dataset
 dataset <- read.csv("Data.csv")
 
-X <- dataset[,1:3]
-y <- dataset[,4]
+dataset$Age <- ifelse(is.na(dataset$Age),
+                  ave(dataset$Age, FUN = function(x) mean(dataset$Age, na.rm = T)),
+                  dataset$Age)
+
+dataset$Salary <- ifelse(is.na(dataset$Salary),
+                      ave(dataset$Salary, FUN = function(x) mean(dataset$Salary, na.rm = T)),
+                      dataset$Salary)
+
+# Split into training and test set
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set <- dataset[split,]
+test_set <- dataset[!split,]
+
+# Feature scaling
+training_set[,2:3] <- scale(training_set[,2:3])
+test_set[,2:3] <- scale(test_set[,2:3])
