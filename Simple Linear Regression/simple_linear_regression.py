f702aee696f30f4c059f2835a15aaf755f2590f6
kwichmann <kristian.wichmann@gmail.com>
2016-11-24 15:17:47 +0100

    Minor progress

--- Simple Linear Regression/simple_linear_regression.py
+++ Simple Linear Regression/simple_linear_regression.py
@@ -9,8 +9,8 @@ import pandas as pd
 dataset = pd.read_csv('Salary_Data.csv')
 
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Split into training and test sets
 from sklearn.cross_validation import train_test_split
-X_train, y_train, X_test, y_test = train_test_split(X, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
